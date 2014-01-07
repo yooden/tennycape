46b7d06e9f0bfcb237e3eb58cf2edd8262507500
yooden <yooden@tinkerbell.yoo.local>
2014-01-07 23:23:57 +0100

    Added db user creation script

---
+++ db/createuser.sh
@@ -0,0 +1,8 @@
+#!  /bin/sh
+
+#   run as user postgres
+#
+#   My installation uses either "peer" or "m5", but still does not ask for
+#   a password (as promised by createuser's man page).
+
+createuser --createdb --encrypted --pwprompt --no-createrole --no-superuser river
