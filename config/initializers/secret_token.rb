7351f2b830787dd7236000437897ba690cb56066
Yooden Vranx <yooden@tinkerbell.yoo.local>
2014-01-07 22:50:19 +0100

    initial state after getting 'rails server' to start (needed Javascript)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+River::Application.config.secret_token = 'd2b59037095b2d02a8aa72bfb7d87a3be3858f87bb39e8267e9243a1e803426c78a9d259703f0b7e18e6bbb65ee511e6c5727726ec14609617c135bff6c9bf95'
