8663539ab89b9a4b55a49534a256d447996e4029
yooden <yooden@gmx.net>
2014-01-13 19:34:26 +0100

    First shot at putting some test data in the tables (non-functional).

---
+++ db/river-testdata.sql
@@ -0,0 +1,13 @@
+#   River test data
+
+insert into address (coordinates, street, city, zip, country)
+values ('TODO', 'Mülheimer Straße 127', 'Köln', '51063', 'Deutschland');
+
+insert into venue (name, address, size)
+values ('Gebäude 9', TODO:AddressID, '0');
+
+insert into artist (name)
+values ('Future of the Left');
+
+insert into event (name, venue, artist, date)
+values ('', TODO:VenueID, TODO:ArtistID, '2000-01-01');
