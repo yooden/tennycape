412513261ce54b1a8113210f78fdc460cbf5a424
yooden <yooden@gmx.net>
2015-02-18 19:07:53 +0100

    raked adress table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140316102236) do
+ActiveRecord::Schema.define(:version => 20140328165353) do
+
+  create_table "addresses", :force => true do |t|
+    t.string   "street"
+    t.string   "city"
+    t.integer  "zip"
+    t.string   "country"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "artists", :force => true do |t|
     t.string   "name"
